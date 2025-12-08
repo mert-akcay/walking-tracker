6cd8756a461c6883cdcd8c2ccac141955027220a
mert-akcay <akcaymert603@gmail.com>
2025-12-09 02:20:50 +0300

    chore: Update application and Docker port configuration from 3000 to 2972.

--- run_docker.ps1
+++ run_docker.ps1
@@ -9,4 +9,4 @@ docker build -t walking-tracker .
 # Note: If your DATABASE_URL uses 'localhost', you may need to change it to 'host.docker.internal'
 # to access the database running on your host machine.
 Write-Host "Starting container..."
-docker run -p 3000:3000 --env-file .env --name walking-tracker walking-tracker
+docker run -p 2972:2972 --env-file .env --name walking-tracker walking-tracker
